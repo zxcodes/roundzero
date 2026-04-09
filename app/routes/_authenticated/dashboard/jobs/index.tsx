611332c0c3b26315f24aafb755794a6ea6762a4c
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-04-09 07:28:49 +0530

    wip

--- app/routes/_authenticated/dashboard/jobs/index.tsx
+++ app/routes/_authenticated/dashboard/jobs/index.tsx
@@ -267,12 +267,12 @@ function ActiveJobsTable({
                         Publish
                       </Button>
                     ) : null}
-                    <Button variant="ghost" size="sm" asChild>
+                    <Button variant="outline" size="sm" asChild>
                       <Link to="/dashboard/job-applicants/$jobId" params={{ jobId: job.id }}>
                         Applicants
                       </Link>
                     </Button>
-                    <Button variant="ghost" size="sm" asChild>
+                    <Button variant="outline" size="sm" asChild>
                       <Link to="/dashboard/jobs/$jobId" params={{ jobId: job.id }}>
                         View
                       </Link>
