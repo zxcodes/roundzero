5245c436dd16550b171506d0721f3c4491fbddfe
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-04-11 07:51:47 +0530

    wip

--- app/components/ui/drawer.tsx
+++ app/components/ui/drawer.tsx
@@ -61,7 +61,7 @@ function DrawerContent({
         )}
         {...props}
       >
-        <div className="mx-auto mt-4 hidden h-1.5 w-[100px] shrink-0 rounded-full bg-muted group-data-[vaul-drawer-direction=bottom]/drawer-content:block" />
+        <div className="mx-auto mt-4 hidden h-1.5 w-25 shrink-0 rounded-full bg-muted group-data-[vaul-drawer-direction=bottom]/drawer-content:block" />
         {children}
       </DrawerPrimitive.Content>
     </DrawerPortal>
