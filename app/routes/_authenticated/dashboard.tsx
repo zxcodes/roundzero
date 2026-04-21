4d4a555abf379bf426561b8962eb2c8ee48f1f4b
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-04-21 18:23:17 +0530

    wip

--- app/routes/_authenticated/dashboard.tsx
+++ app/routes/_authenticated/dashboard.tsx
@@ -68,7 +68,7 @@ function DashboardLayout() {
             onOpenCommandPalette={onOpenCommandPalette}
           />
           <div className="flex flex-1 flex-col">
-            <div className="flex flex-1 flex-col gap-4 p-4 md:gap-6 md:p-6">
+            <div className="mx-auto flex w-full max-w-7xl flex-1 flex-col gap-4 p-4 md:gap-6 md:p-6">
               <Outlet />
             </div>
           </div>
