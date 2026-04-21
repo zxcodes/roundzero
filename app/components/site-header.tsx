c56c4b4b38e97e7694e0f719eb8fdcc22609fa43
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-04-21 10:45:01 +0530

    wip

--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -30,8 +30,8 @@ export function SiteHeader({
           >
             <HugeiconsIcon icon={Search01Icon} strokeWidth={2} data-icon="inline-start" />
             <span className="text-xs">Search...</span>
-            <kbd className="pointer-events-none ml-2 inline-flex h-5 items-center gap-0.5 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
-              <span className="text-xs">⌘</span>K
+            <kbd className="pointer-events-none ml-2 inline-flex h-5 items-center gap-0.5 rounded-full border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
+              <span className="text-sm">⌘</span>K
             </kbd>
           </Button>
           <Button
