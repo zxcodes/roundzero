33f6c5db427ebee795bb8400e44ae086e55a055a
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-05-01 23:43:32 +0530

    chore: remove cached db instance

--- app/shared/db.ts
+++ app/shared/db.ts
@@ -1,14 +1,9 @@
 import postgres from "postgres";
-import { serverEnv } from "./env.server";
-
-let sql: ReturnType<typeof postgres> | null = null;
 
 export const getDb = () => {
-  if (sql) return sql;
-  sql = postgres(serverEnv.DATABASE_URL, {
-    max: 10,
-    idle_timeout: 30,
+  return postgres(process.env.DATABASE_URL, {
+    max: 1,
+    idle_timeout: 10,
     connect_timeout: 10,
   });
-  return sql;
 };
