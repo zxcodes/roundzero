c7e5b10d91433bd6033e7043980c9ff76629b84b
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-05-05 19:20:11 +0530

    wip

--- app/shared/db.ts
+++ app/shared/db.ts
@@ -1,7 +1,9 @@
 import { env } from "cloudflare:workers";
 import postgres from "postgres";
 
+const MAX_NUMBER_OF_CONNECTIONS = 5;
+
 export const getDb = () => {
   const connectionString = env.HYPERDRIVE.connectionString;
-  return postgres(connectionString);
+  return postgres(connectionString, { max: MAX_NUMBER_OF_CONNECTIONS });
 };
