d88ab3527159fb299ca6070ed4af9a8176f53d69
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-05-05 17:49:37 +0530

    add hyperdrive.

--- app/shared/db.ts
+++ app/shared/db.ts
@@ -1,9 +1,7 @@
+import { env } from "cloudflare:workers";
 import postgres from "postgres";
 
 export const getDb = () => {
-  return postgres(process.env.DATABASE_URL, {
-    max: 1,
-    idle_timeout: 10,
-    connect_timeout: 10,
-  });
+  const connectionString = env.HYPERDRIVE.connectionString;
+  return postgres(connectionString);
 };
