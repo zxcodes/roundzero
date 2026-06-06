b8ec48fb388ffb70ef8f89fa3155d6538eaa2c85
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-06-06 07:45:05 +0530

    enable view transitions.

--- app/router.tsx
+++ app/router.tsx
@@ -40,6 +40,7 @@ export function getRouter() {
     scrollRestoration: true,
     defaultPreload: "intent",
     defaultPreloadStaleTime: 0,
+    defaultViewTransition: true,
     context: {
       user: null,
       isCompany: false,
