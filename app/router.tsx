f5e5b281cdff6388beb7502175111e70d3453af4
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-06-21 19:39:24 +0530

    sentry: enable tracing & disable source map uploads.

--- app/router.tsx
+++ app/router.tsx
@@ -54,6 +54,7 @@ export function getRouter() {
     Sentry.init({
       dsn: "https://93220926b2dbb8136dfb5e8d25f7a3fd@o4511527312687104.ingest.us.sentry.io/4511527318388736",
       sendDefaultPii: true,
+      tracesSampleRate: 1.0,
     });
   }
 
