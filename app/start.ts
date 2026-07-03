dfe8bff8a776ce16444fe8d9dd8be08383587a7b
Mohammed Farmaan <farmaanghouse6@gmail.com>
2026-07-03 14:52:41 +0530

    sentry: add csrf for handler.

--- app/start.ts
+++ app/start.ts
@@ -2,9 +2,13 @@ import {
   sentryGlobalFunctionMiddleware,
   sentryGlobalRequestMiddleware,
 } from "@sentry/tanstackstart-react";
-import { createStart } from "@tanstack/react-start";
+import { createCsrfMiddleware, createStart } from "@tanstack/react-start";
+
+const csrfMiddleware = createCsrfMiddleware({
+  filter: (ctx) => ctx.handlerType === "serverFn",
+});
 
 export const startInstance = createStart(() => ({
-  requestMiddleware: [sentryGlobalRequestMiddleware],
+  requestMiddleware: [sentryGlobalRequestMiddleware, csrfMiddleware],
   functionMiddleware: [sentryGlobalFunctionMiddleware],
 }));
